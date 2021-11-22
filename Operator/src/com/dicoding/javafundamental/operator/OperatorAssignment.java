ef28cb9e0e6641a224b8b719add5c3e66583a470
ZaidHilmi <zaidhilmi15@gmail.com>
2021-11-22 18:39:00 +0700

    Add: Operator

---
+++ Operator/src/com/dicoding/javafundamental/operator/OperatorAssignment.java
@@ -0,0 +1,11 @@
+package com.dicoding.javafundamental.operator;
+
+public class OperatorAssignment {
+    public static void main(String[] args){
+        int value = 2;
+        int anoterValue = 19;
+
+        System.out.println("Data pada value : " + value);
+        System.out.println("Data pada anotherValue : " + anoterValue);
+    }
+}
